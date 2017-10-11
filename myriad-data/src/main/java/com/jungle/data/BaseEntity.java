54c31ee6086d70c0c1cdf15880d4c67d30e0ea33
zhangjun <602902491@qq.com>
2017-10-11 14:57:22 +0800

    新增BaseEntity

--- myriad-data/src/main/java/com/jungle/data/BaseEntity.java
+++ myriad-data/src/main/java/com/jungle/data/BaseEntity.java
@@ -1,10 +1,12 @@
 package com.jungle.data;
 
+import lombok.Data;
+
 /**
  * @author timingbar
  * @created 2017/10/11 0011 下午 2:27
  */
-
+@Data
 public abstract class BaseEntity {
 
 	protected Long id;
